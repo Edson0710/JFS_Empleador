52faec038880a7b7425800040668483fd32f2e29
Edson0710 <edsonnavarro0710@gmail.com>
2019-06-12 23:52:39 -0500

    Login y validacion del correo

--- app/src/main/java/com/example/jfs_proyecto/Login.java
+++ app/src/main/java/com/example/jfs_proyecto/Login.java
@@ -5,33 +5,93 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
-
-
+    EditText correo_etxt, pass_etxt;
+    String correo, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        correo_etxt = findViewById(R.id.Login_etxt_correo);
+        pass_etxt = findViewById(R.id.Login_etxt_contrasena);
+
     }
 
 //----------------------------Botones---------------------------------------------------------------
 
     //Metodo boton registrarse
-    public void Registrarse (View view){
+    public void Registrarse(View view) {
 
-        Intent registrarse = new Intent(Login.this, Registro_1.class);
-        startActivity(registrarse);
+        Intent intent = new Intent(Login.this, Registro_1.class);
+        startActivity(intent);
     }
 
     //Boton iniciar sesion
-    public void Iniciar (View view){
-
-        Intent iniciar = new Intent(Login.this,Vista_principal.class);
-        startActivity(iniciar);
+    public void Iniciar(View view) {
+        correo = correo_etxt.getText().toString().trim();
+        pass = pass_etxt.getText().toString().trim();
+        if (correo.equals("") || pass.equals("")) {
+            Toast.makeText(Login.this, "Llene todos los campos", Toast.LENGTH_SHORT).show();
+        } else {
+            login();
+        }
     }
 
+    //-----------------------------Funciones--------------------------------------------------------
+    //Funcion para login
+    public void login() {
+        String url = "http://192.168.100.80/jfs_proyecto/login_empleador.php?correo=" + correo +
+                "&pass=" + pass;
+        final Intent intent = new Intent(Login.this, Vista_principal.class);
+        JsonObjectRequest peticion = new JsonObjectRequest
+                (
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String valor = response.getString("Estado");
+
+                                    switch (valor) {
+                                        case "EXITOSO":
+                                            startActivity(intent);
+                                            break;
+                                        case "FALLIDO":
+                                            Toast.makeText(Login.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+                                            break;
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        , new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, "Error conexión", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue x = Volley.newRequestQueue(Login.this);
+        x.add(peticion);
+    }
 
 }
--- app/src/main/java/com/example/jfs_proyecto/Registro_1.java
+++ app/src/main/java/com/example/jfs_proyecto/Registro_1.java
@@ -5,32 +5,86 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Registro_1 extends AppCompatActivity {
-
-
-
-
+    EditText correo_etxt;
+    String correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_1);
+
+        correo_etxt = findViewById(R.id.Registro1_etxt_correo);
     }
 
     //-------------------------Botones--------------------------------------------------------------
     //Metodo boton cancelar
-    public void Cancelar (View view){
+    public void Cancelar(View view) {
 
-        Intent cancelar = new Intent(Registro_1.this, Login.class);
-        startActivity(cancelar);
+        Intent intent = new Intent(Registro_1.this, Login.class);
+        startActivity(intent);
     }
 
     //Metodo boton continuar
-    public void Continuar (View view){
-
-        Intent continuar = new Intent(Registro_1.this, Registro_2.class);
-        startActivity(continuar);
+    public void Continuar(View view) {
+        correo = correo_etxt.getText().toString().trim();
+        if (correo.equals("")) {
+            Toast.makeText(Registro_1.this, "Llene todos los campos", Toast.LENGTH_SHORT).show();
+        } else {
+            validacion();
+        }
     }
-    //----------------------------------------------------------------------------------------------
+
+    //---------------------------Funciones----------------------------------------------------------
+    //Metodo para validar si existe el correo
+    public void validacion() {
+        String url = "http://192.168.100.80/jfs_proyecto/validacion_empleador.php?correo=" + correo;
+        final Intent intent = new Intent(Registro_1.this, Registro_2.class);
+        JsonObjectRequest peticion = new JsonObjectRequest
+                (
+                        Request.Method.GET,
+                        url,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String valor = response.getString("Estado");
+
+                                    switch (valor) {
+                                        case "NO":
+                                            startActivity(intent);
+                                            break;
+                                        case "SI":
+                                            Toast.makeText(Registro_1.this, "Este correo ya ha sido registrado", Toast.LENGTH_SHORT).show();
+                                            break;
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        , new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Registro_1.this, "Error conexión", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue x = Volley.newRequestQueue(Registro_1.this);
+        x.add(peticion);
+    }
 }
